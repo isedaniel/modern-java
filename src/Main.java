4b75fdb621129fb2be158ff0197adf0638fcd2a9
Daniel <ise.daniel@gmail.com>
2024-06-07 12:29:09 -0300

    imports changed

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import apple.Apple;
-import apple.AppleColor;
 import processFile.ProcessFile;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
